f3eb0fe1519910fed20bee3f296c3cf389cdf5b2
Justin Pease <justinpease2@gmail.com>
2016-04-20 12:43:13 -0600

    initial commit

---
+++ public/client.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+var statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
+
+socket.on('voteCount', function (votes) {
+  var voteCount = document.getElementById('vote-count')
+  voteCount.innerText = votes["A"] + votes["B"]
+  console.log(votes);
+});
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
